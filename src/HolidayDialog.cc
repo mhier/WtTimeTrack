e22161ab07855e5662eb5375b5a80bf4ea5032b3
Martin Hierholzer <martin@hierholzer.info>
2017-12-22 17:30:15 +0100

    more validation

--- src/HolidayDialog.cc
+++ src/HolidayDialog.cc
@@ -32,6 +32,8 @@ HolidayDialog::HolidayDialog(Updateable *owner, Session &session, Wt::Dbo::ptr<H
     }
 
     auto grid = contents()->setLayout(std::make_unique<Wt::WGridLayout>());
+    grid->setColumnStretch(0,0);
+    grid->setColumnStretch(1,1);
 
     grid->addWidget(std::make_unique<Wt::WText>("Erster Tag: "), 0, 0);
     auto de1 = grid->addWidget(std::make_unique<Wt::WDateEdit>(), 0, 1);
@@ -41,7 +43,8 @@ HolidayDialog::HolidayDialog(Updateable *owner, Session &session, Wt::Dbo::ptr<H
     auto de2 = grid->addWidget(std::make_unique<Wt::WDateEdit>(), 1, 1);
     de2->setDate(holiday_->last);
 
-    contents()->setWidth(600);
+    errorMessage = grid->addWidget(std::make_unique<Wt::WText>(), 8, 1);
+    errorMessage->hide();
 
     if(!createNew) {   // existing absence might be deleted
       Wt::WPushButton *del = footer()->addWidget(std::make_unique<Wt::WPushButton>("Löschen"));
@@ -57,6 +60,22 @@ HolidayDialog::HolidayDialog(Updateable *owner, Session &session, Wt::Dbo::ptr<H
     ok->setDefault(true);
     ok->clicked().connect(this, [=] {
       dbo::Transaction transaction(session_.session_);
+
+      // check if edit is valid
+      if(!de1->date().isValid() || !de2->date().isValid()) {
+        errorMessage->setText("Fehler: Bitte ersten und letzten Tag angeben!");
+        errorMessage->show();
+        return;
+      }
+
+      // check if edit is valid
+      if(de1->date() > de2->date()) {
+        errorMessage->setText("Fehler: Der letzte Tag darf nicht vor dem ersten liegen!");
+        errorMessage->show();
+        return;
+      }
+
+      // update the holiday object
       holiday_.modify()->first = de1->date();
       holiday_.modify()->last = de2->date();
       if(createNew) {   // create new on ok
